dedfa00eed59c20d107a5ecb31cc483eeec75755
CaillouDev <162549652+CaillouDev@users.noreply.github.com>
2024-05-22 12:46:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiMeal = ["Maryline", "Thomas", "Tanguy", "Jérémy V", "Jérémy F", "Quentin", "Tien", "Pierre"];
 const annonceList = ["Le hasard a désigné", "C'est ainsi que le sort désigne", "Tous les regards se posent sur", "Hats down for", "C'est ton tour", "A toi de jouer", "Surprends-nous"];
 const randomBtn = document.getElementById("random-button");
-randomBtn.addEventListener("click",random(apiMeal.length, annonceList.length))
+
 function random (formation, annonce) {
   const randomIndex = Math.floor(Math.random()*formation);
   console.log(randomIndex);
@@ -10,6 +10,8 @@ function random (formation, annonce) {
   alert (`${annonceList[randomAnnonce]} ${apiMeal[randomIndex]}.`);
   return
  }
+
+randomBtn.addEventListener("click",random(apiMeal.length, annonceList.length))
 
 
 
