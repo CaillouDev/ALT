8280add9b04ae5d6976d38424d931a80dbd483ea
CaillouDev <162549652+CaillouDev@users.noreply.github.com>
2024-05-22 12:52:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("test")
+
 const apiMeal = ["Maryline", "Thomas", "Tanguy", "Jérémy V", "Jérémy F", "Quentin", "Tien", "Pierre"];
 const annonceList = ["Le hasard a désigné", "C'est ainsi que le sort désigne", "Tous les regards se posent sur", "Hats down for", "C'est ton tour", "A toi de jouer", "Surprends-nous"];
 const randomBtn = document.getElementById("random-button");
